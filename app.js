4239181bb700a61561e4e0ffb1f3ba31cf370329
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-05 17:23:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -300,7 +300,6 @@ app.post('/menu.html/process', function (req, res) {
 	req.on('end', () => {
 	pdata = qs.parse(pdata);
 	var mail = pdata["email"];
-	mail = mail.split(';')[0];
 	var x = String(pdata['hidden']);
 	//calebs code to add foods the user chooses to their database 
 	//x is the string representing all the foods the user chose
