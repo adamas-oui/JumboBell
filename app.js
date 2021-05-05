980c8862de057b27477725b7b68f75b892c933eb
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-05 16:18:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ app.get('/index.html/process', function (req, res, next) {
 			setTimeout(function(){db.close;}, 2000);
 		}); 
 });
-	app.get('/home.html');
+	res.redirect('/index.html/process/home.html');
+	return;
 });
 app.get('/home.html', function (req, res) {
   file = 'home.html';
