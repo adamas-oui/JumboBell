e8e7cc0c17a1c9c7d53121ab2f4c3ffdccd512c9
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-05 00:06:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/index.html/process', function (req, res, next) {
   req.on('end', () => {
 	pdata = qs.parse(pdata);
 	var Email = pdata["email"];
-	app.set("email", "myemail");
+	app.set("email",String(pdata['email']));
 	
 		MongoClient.connect(urll, { useUnifiedTopology: true }, function(err, db) {
 		console.log("hello");
