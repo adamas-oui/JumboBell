930366ac904ffa61275ad0dfa5af5f51e422981e
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-06 01:23:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/index.html', function (req, res, next) {
       setTimeout(function(){res.end();}, 2000);
     });
 });
-app.post('index.html/process', function (req, res, next) {
+app.get('index.html/process', function (req, res, next) {
 	console.log("Process the form");
 	pdata = "";
 	req.on('data', data => {
