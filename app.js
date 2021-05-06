f1cc856b9ef7f5d946e46acde0241aed0d3993a7
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-06 04:01:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -382,7 +382,7 @@ console.log(useremail);
   secondpart = OGstring.split("foodname=")[2];
 secondpart = decodeURIComponent(secondpart);
   secondpart = secondpart.split(";")[0];
- secondpart = secondpart.split("=")[1];
+ secondpart = secondpart.split("=");
  secondpart.splice(0,1);
  for(var i = 0; i < secondpart.length; i++ ) {
      
