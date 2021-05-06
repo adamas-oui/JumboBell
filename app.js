9d23f1dad1e51c783e80b3064c5707ed8efda85e
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-06 03:40:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -467,10 +467,10 @@ app.get('/account.html/process', function (req, res) {
 	console.log(req.url);
 	console.log(" SHOUld get actual thing: ")
 	var stringURL2 = req.url.toString();
-	console.log(stringURL2 );
+	console.log(stringURL2);
 	
 	stringURL2 = stringURL2.split("=");
-	stringURL2 = stringURL2[1];
+	stringURL2 = String(stringURL2)[1];
 	stringURL2 =  stringURL2.split(";");
 	stringURL2 = stringURL2[0];
 	stringURL2 = decodeURIComponent(stringURL2);
